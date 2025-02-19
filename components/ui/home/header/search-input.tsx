0b99837485a61f09c019e1d998a623192c8cbf03
Priyanshu Ranjan <priyanshuranjan0430@gmail.com>
2025-02-20 01:11:59 +0530

    navbar built

---
+++ components/ui/home/header/search-input.tsx
@@ -0,0 +1,23 @@
+import { Search } from 'lucide-react'
+import React from 'react'
+import { Input } from '../../input'
+
+function SearchInput() {
+    return (
+        <div>
+            <form action="">
+                <div className="relative">
+                    <Search className='absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground' />
+                    <Input
+                        type='text'
+                        placeholder='Search articles...'
+                        name='search'
+                        className='pl-10 w-48 focus-visible::ring-1 '
+                    />
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default SearchInput
