ef64dd248b9584a31555042b0930720aacc11a3f
Priyanshu Ranjan <priyanshuranjan0430@gmail.com>
2025-02-21 10:43:25 +0530

    hero section

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,11 @@
+import HeroSection from "@/components/ui/home/hero-section";
 import Navbar from "@/components/ui/home/header/Navbar";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <HeroSection />
     </div>
   );
 }
